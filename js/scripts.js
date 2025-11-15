a6652eed84a974b74a77bdc07d53c9a37c115fc8
markusl23 <markus4github@riseup.net>
2025-11-15 12:55:18 +0100

    fixed selector issue, causing actions applying to all list items, not just one

--- js/scripts.js
+++ js/scripts.js
@@ -12,19 +12,19 @@ function newItem(){
 
  //2. Crossing out an item from the list of items:
    function crossOut() {
- 		$('li').toggleClass("strike");
+ 		li.toggleClass("strike");
  	}
 
- 	$('li').on("dblclick",crossOut);
+ 	li.on("dblclick",crossOut);
 
  //3(i). Adding the delete button "X": 
    let crossOutButton = $('<crossOutButton>X</crossOutButton>');
  	 li.append(crossOutButton);
 
- 	$('crossOutButton').on("click", deleteListItem);
+ 	crossOutButton.on("click", deleteListItem);
  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
    function deleteListItem(){
- 		$('li').addClass("delete");
+ 		li.addClass("delete");
  	}
  // 4. Reordering the items: 
    $('#list').sortable();
